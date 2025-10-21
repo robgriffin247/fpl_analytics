14f6b2fcb6cb8c6082ffcb795e03bf64ae7db530
Rob Griffin <robgriffin247@hotmail.com>
2025-10-21 22:02:34 +0200

    working with secrets...

--- modal/test_app.py
+++ modal/test_app.py
@@ -1,8 +1,14 @@
 import modal
 import datetime
+import os 
 
 app = modal.App("test-app")
 
-@app.function(schedule=modal.Cron('45 19 21 * *'))
-def print_timestamp():
-    print(f"The time is now {datetime.datetime.now()}")
+@app.function(
+        schedule=modal.Cron('*/1 * * * *'),
+        secrets=[modal.Secret.from_name("motherduck-secret")],
+    )
+def run_pipeline():
+    print(f"The time is now {datetime.datetime.now()}: {os.getenv("MOTHERDUCK_TOKEN")[0:10]}")
+
+    
