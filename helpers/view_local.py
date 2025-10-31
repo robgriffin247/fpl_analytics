3a06ab44f6e18a469b452324b64e8a548832e722
Rob Griffin <robgriffin247@hotmail.com>
2025-10-31 14:46:36 +0100

    clean up

--- helpers/view_local.py
+++ helpers/view_local.py
@@ -1,4 +1,5 @@
 import duckdb
 
 with duckdb.connect("data/fpl_analytics.duckdb") as con:
-    print(con.sql("select * from core.obt_player_gameweek_stats"))
+    # print(con.sql("select * from core.obt_player_gameweek_stats"))
+    print(con.sql("select to_timestamp(max(load_id::double)) from fpl._dlt_loads"))
